8d72b1c88eda0bdc8186e380bf260d9230b4815e
COBOS FRANCO INC <jorgecobos_franco@hotmail.com>
2023-01-30 20:21:25 -0500

    Create mainmenustatusenum.cs

---
+++ src/services/models/mainmenustatusenum.cs
@@ -0,0 +1,12 @@
+namespace Marketplace.SaaS.Accelerator.Services.Models;
+
+/// <summary>
+/// Sets Subscription Operation Status.
+/// </summary>
+public enum MainMenuStatusEnum
+{
+    /// <summary>
+    /// Is Active Enable
+    /// </summary>
+    IsLicenseManagementEnabled,
+}
